f26649db06e831cf0779e63a80ffe04f4d6718f2
isaacpitblado <isaacpitblado@tamu.edu>
2021-03-06 18:55:34 -0600

    initial commit

---
+++ src/test/java/com/isaacpit/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaacpit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class DemoApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/isaacpit/restservice/GreetingControllerTests.java
@@ -0,0 +1,13 @@
+package com.isaacpit.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingControllerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
